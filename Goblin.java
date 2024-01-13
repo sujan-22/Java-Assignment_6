400b9a5280f219f5ffac21cf6ff1cbd5ab59eaa8
Sujan <120612188+sujan-22@users.noreply.github.com>
2024-01-12 23:27:38 -0500

    Update Goblin.java

--- Goblin.java
+++ Goblin.java
@@ -1,33 +1,64 @@
-//My name: Sujan Rokad, 000882948
-//My partner's name: Jay Patel, 000881881
-//NUmber of total hours I worked: Around 17
-//I think we did same effort in almost everything. (50%-50%)
+/*
+ * Author:                 Sujan Rokad, 000882948
+ *                         Jay Patel, 000881881 (Partner's Name and ID)
+ * Authorship statement:   I, Sujan Rokad, 000882948, and my partner, Jay Patel, 000881881, certify that this material is our original work.
+ *                         No other person's work has been used without due acknowledgment.
+ * Purpose:                Define the Goblin class, a subclass of Monster, with additional functionality for attacking, resting, and more.
+ */
 
 package Assignment_6;
 
 public class Goblin extends Monster {
-   private Goblin enemy; //Variable enemy
-    // constructor with all parameters
+    private Goblin enemy; // Variable enemy
+
+    /**
+     * Constructor for the Goblin class with all parameters.
+     *
+     * @param clanAffiliation The clan affiliation of the goblin.
+     * @param ferocity        The ferocity score of the goblin.
+     * @param defense         The defense score of the goblin.
+     * @param magic           The magic score of the goblin.
+     * @param treasure        The treasure score of the goblin.
+     * @param health          The health score of the goblin.
+     * @param enemy           The enemy goblin.
+     */
     public Goblin(String clanAffiliation, int ferocity, int defense, int magic, int treasure, int health, Goblin enemy) {
-        super(clanAffiliation, ferocity, defense, magic, treasure, health); // call superclass constructor
+        super(clanAffiliation, ferocity, defense, magic, treasure, health); // Call superclass constructor
         this.enemy = enemy;
     }
 
+    /**
+     * Get the enemy goblin.
+     *
+     * @return The enemy goblin.
+     */
     public Goblin getEnemy() {
         return enemy;
     }
 
+    /**
+     * Set the enemy goblin.
+     *
+     * @param enemy The enemy goblin.
+     */
     public void setEnemy(Goblin enemy) {
         this.enemy = enemy;
     }
 
-    // calculate the goblin's attack score, taking into account agility and rider
+    /**
+     * Calculate the goblin's attack score, taking into account ferocity and magic.
+     *
+     * @return The calculated attack score.
+     */
     public double calculateAttackScore() {
-        double attackScore = getFerocity() + getMagic();
-        return attackScore;
+        return getFerocity() + getMagic();
     }
 
-    // attack another monster, reducing its health based on attack score and defense score
+    /**
+     * Attack another monster, reducing its health based on attack score and defense score.
+     *
+     * @param other The target monster to be attacked.
+     */
     public void attack(Monster other) {
         double attackScore = calculateAttackScore();
         double defenseScore = other.getDefense() + other.getMagic();
@@ -40,29 +71,47 @@ public class Goblin extends Monster {
         }
     }
 
+    /**
+     * Rest action for the goblin, restoring lost health.
+     */
     public void rest() {
-        // Calculate the amount of health lost during resting
-        int lostHealth = Math.max((int) Math.round(getMaxHealth() * 0.1), 1); // lost health during resting is 10% of max health, at least 1
-
-        // Restore lost health, but not more than max health
-        setHealth(Math.min(getHealth() + lostHealth, getMaxHealth())); // restore lost health, but not more than max health
-
-        // If this object is mounted and has a rider, let the rider rest too
+        int lostHealth = Math.max((int) Math.round(getMaxHealth() * 0.1), 1); // Lost health during resting is 10% of max health, at least 1
+        setHealth(Math.min(getHealth() + lostHealth, getMaxHealth())); // Restore lost health, but not more than max health
     }
 
+    /**
+     * Get the name of the goblin.
+     *
+     * @return The name of the goblin.
+     */
     @Override
     public String getName() {
         return getName();
     }
 
+    /**
+     * Get the maximum health of the goblin.
+     *
+     * @return The maximum health of the goblin.
+     */
     private int getMaxHealth() {
         return getMaxHealth();
     }
 
+    /**
+     * Calculate the goblin's defense score.
+     *
+     * @return The calculated defense score.
+     */
     public double calculateDefenseScore() {
         return calculateDefenseScore();
     }
 
+    /**
+     * Check if the goblin is defeated.
+     *
+     * @return True if the goblin is defeated, false otherwise.
+     */
     public boolean isDefeated() {
         return isDefeated();
     }
