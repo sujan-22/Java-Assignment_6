11a66e0e229c3e93e3b7c8e64617de20f78410e7
Sujan <120612188+sujan-22@users.noreply.github.com>
2024-01-12 23:28:28 -0500

    Update Infantry.java

--- Infantry.java
+++ Infantry.java
@@ -1,7 +1,10 @@
-//My name: Sujan Rokad, 000882948
-//My partner's name: Jay Patel, 000881881
-//NUmber of total hours I worked: Around 17
-//I think we did same effort in almost everything. (50%-50%)
+/*
+ * Author:                 Sujan Rokad, 000882948
+ *                         Jay Patel, 000881881 (Partner's Name and ID)
+ * Authorship statement:   I, Sujan Rokad, 000882948, and my partner, Jay Patel, 000881881, certify that this material is our original work.
+ *                         No other person's work has been used without due acknowledgment.
+ * Purpose:                Define the Infantry class, a subclass of Monster, with additional functionality for attack, rest, and more.
+ */
 
 package Assignment_6;
 
@@ -9,39 +12,68 @@ class Infantry extends Monster {
 
     private Orc warlord; // A reference to the Infantry's Warlord
 
-    // Constructor with specified parameters
+    /**
+     * Constructor for the Infantry class with specified parameters.
+     *
+     * @param clanAffiliation The clan affiliation of the infantry.
+     * @param ferocity        The ferocity score of the infantry.
+     * @param defense         The defense score of the infantry.
+     * @param magic           The magic score of the infantry.
+     * @param treasure        The treasure score of the infantry.
+     * @param health          The health score of the infantry.
+     * @param warlord         The warlord of the infantry.
+     */
     public Infantry(String clanAffiliation, int ferocity, int defense, int magic, int treasure, int health, Warlord warlord) {
         super(clanAffiliation, ferocity, defense, magic, treasure, health); // Calls superclass constructor with specified parameters
         this.warlord = warlord; // Calls superclass constructor with specified parameters
         // Adds the Infantry to the Warlord's list of infantry if the Warlord is not null
-        if (warlord != null){
+        if (warlord != null) {
             warlord.addInfantry(this);
         }
     }
 
-    // Constructor with default random values
+    /**
+     * Constructor for the Infantry class with default random values.
+     *
+     * @param clanAffiliation The clan affiliation of the infantry.
+     */
     public Infantry(String clanAffiliation) {
-        this(clanAffiliation, (int)(Math.random()*11), (int)(Math.random()*11), (int)(Math.random()*11), 0, 5,null);
+        this(clanAffiliation, (int) (Math.random() * 11), (int) (Math.random() * 11), (int) (Math.random() * 11), 0, 5, null);
     }
 
-    // Getter method for the Infantry's warlord
+    /**
+     * Get the warlord of the infantry.
+     *
+     * @return The warlord of the infantry.
+     */
     public Orc getWarlord() {
         return warlord;
     }
 
-    // Setter method for the Infantry's warlord
+    /**
+     * Set the warlord of the infantry.
+     *
+     * @param warlord The warlord of the infantry.
+     */
     public void setWarlord(Orc warlord) {
         this.warlord = warlord;
     }
 
-    // Calculates the Infantry's attack score, taking into account its ferocity, magic, and its Warlord's leadership rating
+    /**
+     * Calculate the Infantry's attack score, taking into account its ferocity, magic, and its Warlord's leadership rating.
+     *
+     * @return The calculated attack score.
+     */
     @Override
     public double calculateAttackScore() {
         return (getFerocity() + getMagic()) * (1.0 + (warlord != null ? warlord.getLeadershipRating() / 10.0 : 0));
     }
 
-    // Overrides the super class's attack method to factor in the Infantry's attack score and the target Monster's
-    // defense score
+    /**
+     * Attack another monster, reducing its health based on attack score and defense score.
+     *
+     * @param other The target monster to be attacked.
+     */
     @Override
     public void attack(Monster other) {
         double attackScore = calculateAttackScore();
@@ -55,40 +87,60 @@ class Infantry extends Monster {
         }
     }
 
-    // Overrides the superclass's rest method to increase the Infantry's health by 1, up to its maximum health of 5
+    /**
+     * Rest action for the infantry, increasing health by 1, up to its maximum health of 5.
+     */
     @Override
     public void rest() {
         setHealth(Math.min(getHealth() + 1, getMaxHealth()));
     }
 
-    // Returns the Infantry's maximum health of 5
+    /**
+     * Get the maximum health of the infantry.
+     *
+     * @return The maximum health of the infantry.
+     */
     private int getMaxHealth() {
         return 5;
     }
 
-    // Returns the Infantry's name of "Infantry"
+    /**
+     * Get the name of the infantry.
+     *
+     * @return The name of the infantry.
+     */
     @Override
     protected String getName() {
         return "Infantry";
     }
 
-    // Calculates the defense score and returns score to the monster
+    /**
+     * Calculate the defense score of the infantry.
+     *
+     * @return The calculated defense score.
+     */
     public double calculateDefenseScore() {
-
         return (getDefense() + getMagic()) * (1.0 + (warlord != null ? warlord.getLeadershipRating() / 10.0 : 0));
     }
 
-    // Boolean value that checks if monster is defeated or not
+    /**
+     * Check if the infantry is defeated.
+     *
+     * @return True if the infantry is defeated, false otherwise.
+     */
     public boolean isDefeated() {
-
         return getHealth() == 0;
     }
 
-    // String representation for Infantry
+    /**
+     * String representation for the infantry.
+     *
+     * @return A string representation of the infantry.
+     */
+    @Override
     public String toString() {
         StringBuilder sb = new StringBuilder(super.toString());
         sb.append("\nWarlord: ").append(warlord != null ? warlord.getClanAffiliation() : "None");
         return sb.toString();
     }
 }
-
