36062bf68d85ce3f9f817ca90995a84fb56fb46f
Sujan <120612188+sujan-22@users.noreply.github.com>
2024-01-12 23:35:14 -0500

    Update Warlord.java

--- Warlord.java
+++ Warlord.java
@@ -1,24 +1,38 @@
-//My name: Sujan Rokad, 000882948
-//My partner's name: Jay Patel, 000881881
-//NUmber of total hours I worked: Around 17
-//I think we did same effort in almost everything. (50%-50%)
-
-package Assignment_6;
-
-import java.util.ArrayList;
+/*
+ * Author:                 Sujan Rokad, 000882948
+ *                         Jay Patel, 000881881 (Partner's Name and ID)
+ * Authorship statement:   I, Sujan Rokad, 000882948, and my partner, Jay Patel, 000881881, certify that this material is our original work.
+ *                         No other person's work has been used without due acknowledgment.
+ * Purpose:                Define an abstract class representing Warlords in the game, extending the Orc class.
+ */
 
 abstract class Warlord extends Orc {
 
-    // Constructor with all parameters
+    /**
+     * Constructor for creating a Warlord with specified attributes.
+     *
+     * @param clanAffiliation The clan affiliation of the Warlord.
+     * @param ferocity        The ferocity score of the Warlord.
+     * @param defense         The defense score of the Warlord.
+     * @param magic           The magic score of the Warlord.
+     * @param treasure        The amount of treasure the Warlord has.
+     * @param health          The health score of the Warlord.
+     * @param leadershipRating The leadership rating of the Warlord (1-5).
+     * @param infantryList    The list of Infantry units under the Warlord.
+     */
     public Warlord(String clanAffiliation, int ferocity, int defense, int magic, int treasure, int health,
                    int leadershipRating, ArrayList<Infantry> infantryList) {
         super(clanAffiliation, ferocity, defense, magic, treasure, health, true, leadershipRating, new ArrayList<>());
     }
 
-    // It also generates a random leadership rating for the Warlord, between 1 and 5 (inclusive)
-    public Warlord(String clanAffiliation) {        super(clanAffiliation);
+    /**
+     * Constructor for creating a Warlord with random attribute values.
+     *
+     * @param clanAffiliation The clan affiliation of the Warlord.
+     */
+    public Warlord(String clanAffiliation) {
+        super(clanAffiliation);
         setWarlord(true);
-        setLeadershipRating((int)(Math.random()*5)+1);
+        setLeadershipRating((int)(Math.random() * 5) + 1);
     }
 }
-
